e68932bb9747856eaa65718703b3cef3229cc14f
nataliemallen <86844435+nataliemallen@users.noreply.github.com>
2024-06-29 12:18:38 -0400

    Update ngsadmix.sh

--- Analysis/Admix/ngsadmix.sh
+++ Analysis/Admix/ngsadmix.sh
@@ -1,35 +1,31 @@
 #!/bin/bash
-#SBATCH --job-name=admix
-#SBATCH -A johnwayne
+#SBATCH -A fnrdewoody
+#SBATCH --job-name=admix_test
 #SBATCH -N 1
 #SBATCH -n 64
 #SBATCH -t 14-00:00:00
-#SBATCH --error=admix_ins.err
-#SBATCH --output=admix_ins.out
-#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH -e %x_%j.err
+#SBATCH -o %x_%j.out
 #SBATCH --mail-user=allen715@purdue.edu
+#SBATCH --mail-type=END,FAIL
 
 module load biocontainers
 module load angsd
 
 reference_genome="/scratch/negishi/allen715/Gray_whales/reference/ref.fa"
 fai_file="/scratch/negishi/allen715/Gray_whales/reference/ref.fa.fai"
-##used beagle from PCA
-beagle="/scratch/negishi/allen715/Gray_whales/PCA/whole_genome.beagle.gz" # Path to the Beagle file
+beagle="/scratch/negishi/allen715/Gray_whales/final_admix/whale.beagle.gz" # Path to the Beagle file
 
-#with beagle file, run ngsadmix for k=2
-#output_base_ngsadmix.qopt will contain 1 row for each individual and one column for each k 
-mkdir k2
-cd k2
+cd ADX
 
-NGSadmix -likes "$beagle" -K 2 -o output_base_ngsadmix -P 64
+# Loop over K values
+for K in 1 2 3 4 5; do
+  # Loop to run NGSadmix 20 times for each K
+  for i in {1..20}; do
+    # Construct the output filename
+    output_file="admix_K${K}_run${i}"
+    # Run NGSadmix
+    NGSadmix -likes "$beagle" -K $K -o $output_file -P 64
+  done
+done
 
-cd ..
-mkdir k3
-cd k3
-NGSadmix -likes "$beagle" -K 3 -o output_base_ngsadmix -P 64
-
-cd ..
-mkdir k4
-cd k4
-NGSadmix -likes "$beagle" -K 4 -o output_base_ngsadmix -P 64
