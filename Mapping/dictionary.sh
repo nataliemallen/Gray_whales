61b8fbc4df1f4bbff9b124697e6dbc66c62ede63
nataliemallen <86844435+nataliemallen@users.noreply.github.com>
2024-07-16 13:56:42 -0400

    Update dictionary.sh

--- Mapping/dictionary.sh
+++ Mapping/dictionary.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 #SBATCH --job-name=whale_dictionary
-#SBATCH -A fnrpredator
+#SBATCH -A fnrdewoody
 #SBATCH -N 1
-#SBATCH -n 64
+#SBATCH -n 128
 #SBATCH -t 12-00:00:00
 #SBATCH --error=dict.err
 #SBATCH --output=dict.out
-#SBATCH --job-name=produce_align_SLURMM_jobs
 #SBATCH --mail-type=START,END,FAIL
 #SBATCH --mail-user=allen715@purdue.edu
 
-module load bioinfo
+module load biocontainers
 module load bwa
-module load picard-tools
+module load picard
 module load samtools
 
-REF=/scratch/bell/allen715/Gray_whales/Reference/original.fa
+#REF=/scratch/bell/allen715/Gray_whales/Reference/original.fa
+
+#bwa index $REF
+#samtools faidx $REF
+#PicardCommandLine CreateSequenceDictionary reference=$REF output=original.fa.dict
+
+REF=/scratch/negishi/allen715/Gray_whales/reference/ref_100kb.fa
 
 bwa index $REF
 samtools faidx $REF
-PicardCommandLine CreateSequenceDictionary reference=$REF output=original.fa.dict
+PicardCommandLine CreateSequenceDictionary reference=$REF output=ref.fa.dict
