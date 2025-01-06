3ccedf38fbc668167aee44b6894408a7824631ee
Natalie Allen <86844435+nataliemallen@users.noreply.github.com>
2025-01-06 13:25:51 -0500

    Update LD_prune.sh

--- Analysis/LD_prune.sh
+++ Analysis/LD_prune.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -A fnrdewoody
-#SBATCH --job-name=LD_prune
+#SBATCH --job-name=LD_prune_west
 #SBATCH -N 1
 #SBATCH -n 64
 #SBATCH -t 12-00:00:00
@@ -9,21 +9,21 @@
 #SBATCH --mail-user=allen715@purdue.edu
 #SBATCH --mail-type=END,FAIL
 
-### prune LD
+## prune LD
 
 module purge
 module load biocontainers
 module load angsd
 module load ngsld
 
-bam lists for east vs west
-EAST="east_bamlist.txt"
-
-EAST_OUT="/scratch/negishi/allen715/Gray_whales/PCA/LD_pruned/east"
+# Set your input BAM file list
+BAM_LIST="/scratch/negishi/allen715/Gray_whales/final_bams/merged/final_merged_use/new_merged_bams.list"
+# Set the reference genome FASTA file path
 REF="/scratch/negishi/allen715/Gray_whales/reference/ref.fa"
+# Set output directory
+OUT="/scratch/negishi/allen715/Gray_whales/PCA/LD_pruned/all/whales"
 
-# run angsd
-angsd -bam "$EAST" \
+angsd -bam "$BAM_LIST" \
       -doCounts 1 \
       -GL 1 \
       -doGlf 2 \
@@ -36,39 +36,73 @@ angsd -bam "$EAST" \
       -baq 1 \
       -ref "$REF" \
       -nThreads 60 \
-      -out "$EAST_OUT" \
+      -out "$OUT" \
 
-zcat east.beagle.gz | cut -f 1 | tail -n +2 | sed 's/_/\t/' > east.pos.txt
+zcat whales.beagle.gz | cut -f 1 | tail -n +2 | sed 's/_/\t/' > whales.pos.txt
 
-# calculate ld for EAST
+#calculate ld for whales
 east_IND=$(wc -l < east_bamlist.txt)
-less east.beagle.gz --pos | cut -f1 | sed 's/1_/1\t/g' | sed '1d' > east.sites.txt
-east_SITES=$(less east.sites.txt | wc -l)
+less whales.beagle.gz --pos | cut -f1 | sed 's/1_/1\t/g' | sed '1d' > whales.sites.txt
+east_SITES=$(less whales.sites.txt | wc -l)
 
-# check for extra line in sites file, do this, then redo site number calculation
-head -n -1 east.sites.txt > temp.txt ; mv temp.txt east.sites.txt
+#if problem with extra line in sites file, do this, then redo site number calculation
+head -n -1 whales.sites.txt > temp.txt ; mv temp.txt whales.sites.txt
 
-ngsLD --geno east.beagle.gz --probs \
-    --pos east.sites.txt \
+ngsLD --geno whales.beagle.gz --probs \
+    --pos whales.sites.txt \
     --max_kb_dist 10 --min_maf 0.05 --extend_out \
         --N_thresh 0.3 --call_thresh 0.9 \
         --n_threads 60 --verbose 1 \
-        --n_ind 33 --n_sites 2301096 | \
-             sort -k 1,1Vr -k 2,2V > east.allSNPs.ld
+        --n_ind 74 --n_sites 2465830 | \
+             sort -k 1,1Vr -k 2,2V > whales.allSNPs.ld
+            
+# reference
+REF="/scratch/negishi/allen715/Gray_whales/reference/ref.fa"
+# create contig_lengths.txt from .fai file
+awk '{print $1, $2}' "${REF}.fai" > contig_lengths.txt
+# extract contig names from whale.pos.txt
+cut -f 1 whales.sites.txt | sort -u > contig_list.txt
 
-# run prune_graph 
-/home/allen715/prune_graph/target/release/prune_graph --in east.allSNPs.ld \
+# split contigs into "large" and "small"
+awk '{if ($2 > 85000000) print $1 > "large_contigs.txt"; else print $1 > "small_contigs.txt"}' contig_lengths.txt
+
+# process large contigs 
+while read -r contig; do
+    grep -w "$contig" whales.allSNPs.ld > "${contig}.ld"
+    /home/allen715/prune_graph/target/release/prune_graph --in "${contig}.ld" \
+        --weight-field "column_7" \
+        --weight-filter "column_3 <= 10000 && column_7 >= 0.5" \
+        --n-threads 40 \
+        --out "${contig}_unlinked.ld"
+done < large_contigs.txt
+
+# combine large contig results
+cat *_unlinked.ld > large_contigs_unlinked.ld
+
+# process small contigs 
+grep -wf small_contigs.txt whales.allSNPs.ld > small_contigs.ld
+/home/allen715/prune_graph/target/release/prune_graph --in small_contigs.ld \
     --weight-field "column_7" \
     --weight-filter "column_3 <= 10000 && column_7 >= 0.5" \
-    --out east_unlinked.ld
+    --n-threads 40 \
+    --out small_contigs_unlinked.ld
+
+# combine all results 
+cat large_contigs_unlinked.ld small_contigs_unlinked.ld > whales_unlinked.ld
+
+# make a copy 
+cp whales_unlinked.ld whales_unlinked_copy.ld
 
 # make output compatible with beagle
-sed 's/:/_/g' east_unlinked.ld > east_unlinked
+sed 's/:/_/g' whales_unlinked.ld > whales_unlinked
 
-zcat east.beagle.gz | head -n 1 > east_unlinked.beagleheader  # Extract the header
-zcat east.beagle.gz | grep -Fwf east_unlinked > east_unlinked.beagle  # Keep only unlinked loci
-cat east_unlinked.beagleheader east_unlinked.beagle | gzip > east_unlinked.beagle.gz  # Combine header and SNP data
+zcat whales.beagle.gz | head -n 1 > whales_unlinked.beagleheader  
+zcat whales.beagle.gz | grep -Fwf whales_unlinked > whales_unlinked.beagle  
+cat whales_unlinked.beagleheader whales_unlinked.beagle | gzip > whales_unlinked.beagle.gz  
 
-pcangsd -b east_unlinked.beagle.gz -o east_pca -t ${N}
-cat east_unlinked | wc -l  # Count the number of unlinked loci
-zcat east_unlinked.beagle.gz | tail -n +2 | wc -l  # Check the number of rows (SNPs) in the final `beagle` file
+module load pcangsd
+
+pcangsd -b whales_unlinked.beagle.gz -o whale_pca -t 64
+cat whales_unlinked | wc -l  # Count the number of unlinked loci
+zcat whales_unlinked.beagle.gz | tail -n +2 | wc -l  # Check the number of rows (SNPs) in the final `beagle` file
+
